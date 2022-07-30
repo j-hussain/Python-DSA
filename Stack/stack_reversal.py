99e5daeae2aa32166ecfa0d616d249f8398bf91a
Jabir Hussain <jabir-hussain@hotmail.co.uk>
2022-07-30 18:16:34 +0400

    simple stack reversal app

---
+++ Stack/stack_reversal.py
@@ -0,0 +1,13 @@
+# we can use the LIFO protocol to reverse a data sequence
+import stack
+
+def reverse_file(filename):
+    new_stack = stack.Stack()
+    with open(filename, "r") as f:
+        for line in f:
+            new_stack.push(line.rstrip("\n"))
+
+    with open(filename, "w") as second:
+        while not stack.is_empty():
+            second.write(new_stack.pop() + "\n")
+
